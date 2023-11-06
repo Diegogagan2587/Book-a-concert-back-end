1e7e36e26842560cec5340a0ea51a4eed48c634e
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-11-06 09:12:38 -0600

    Add a seed for initial data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,27 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+#Ariaga Creates an account and create an Indepentent Concert
+
+@user_one = User.create(name: "Ariana Grande")
+
+@ariana_concert = Concert.create(
+  title: "Ariana Grande Live Concert",
+  organizer: @user_one,
+  description: "Ariana Concert: Ariana Grande is an American singer, songwriter, and actress. A multi-platinum, Grammy Award-winning recording artist, she is known for her wide vocal range, which critics have often compared to that of Mariah Carey.",
+  img: "https://www.billboard.com/files/styles/900_wide/public/media/Ariana-Grande-2019-cr-Getty-billboard-1548.jpg",
+  price: 100,
+  date: "2021-10-10",
+  city: "New York",
+)
+
+# Then anohther user creates an account and buys a ticket or reserve for the concert
+
+@user_two = User.create(name: "Alejandro Magnus")
+@reservation = Reservation.create(
+  user: @user_two,
+  concert: @ariana_concert,
+  date: @ariana_concert.date,
+  city: @ariana_concert.city,
+)
