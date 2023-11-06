1b357ccb3ecab962054c4c2397b7abd800b58cd5
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-11-06 09:17:51 -0600

    Fix Refence to Organize wich is the User model

--- db/migrate/20231104011046_create_concerts.rb
+++ db/migrate/20231104011046_create_concerts.rb
@@ -2,7 +2,7 @@ class CreateConcerts < ActiveRecord::Migration[7.1]
   def change
     create_table :concerts do |t|
       t.string :title
-      t.references :organizer, null: false, foreign_key: true
+      t.references :organizer, null: false, foreign_key: { to_table: :users }
       t.text :description
       t.string :img
       t.decimal :price, precision: 9, scale: 2
