f7e8ee2021cb0d252057038fa671b4d49e2d87d6
dani <danifromecuador@gmail.com>
2023-11-06 17:01:56 -0500

    Add gem 'rack-cors' to allow the backend to receive requests

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,16 +1,9 @@
-# Be sure to restart your server when you modify this file.
+# config/initializers/cors.rb
 
-# Avoid CORS issues when API is called from the frontend app.
-# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin Ajax requests.
-
-# Read more: https://github.com/cyu/rack-cors
-
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins "example.com"
-#
-#     resource "*",
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+# Be sure to replace 'http://localhost:5173' with the actual origin of your React app.
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins '*'
+    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options]
+  end
+end
