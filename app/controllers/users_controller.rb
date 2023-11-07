b6f3c730074a0c34390ef81045f08cefc2f86288
dani <danifromecuador@gmail.com>
2023-11-07 12:05:18 -0500

    Avoid creating repeated users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,11 @@
 class UsersController < ApplicationController
   def create
     user = User.new(user_params)
+
     if user.save
       render json: { message: 'User created successfully' }
+    elsif user.errors[:name].include?('has already been taken')
+      render json: { error: 'User with this name already exists' }, status: :unprocessable_entity
     else
       render json: { error: user.errors.full_messages.join(', ') }, status: :unprocessable_entity
     end
