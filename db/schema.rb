a331cf01e02d792b3f1278471f5aee5dc5d2a881
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-12-27 07:14:16 -0600

    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_06_150551) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_27_131234) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,6 +43,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_06_150551) do
     t.boolean "logged", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
+    t.string "password"
   end
 
   add_foreign_key "concerts", "users", column: "organizer_id"
