de52c6020fd8cbaa5d9d6ef01a03e16dce4e167f
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-12-27 07:36:36 -0600

    use port 3001 instead of 3000

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -310,4 +310,6 @@ Devise.setup do |config|
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3001 }
 end
