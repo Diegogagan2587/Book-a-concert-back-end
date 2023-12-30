d688f04885bd5491c05f8465d517803c967c392d
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-12-30 00:01:35 -0600

    feat(authentication): Sanitize or permit name on sign up and/or account update.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::API
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   def current_user
     @current_user = User.find_by(logged: true)
     if @current_user.nil?
@@ -6,5 +8,16 @@ class ApplicationController < ActionController::API
     else
       render json: @current_user, status: :ok
     end
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(
+      :sign_up, keys: %i[name]
+    )
+    devise_parameter_sanitizer.permit(
+      :account_update, keys: %i[name]
+    )
   end
 end
