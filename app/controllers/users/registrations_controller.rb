2691fa0f8b92e36a915cee55a9ec3bd1c938ece2
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-12-30 11:11:51 -0600

    Fix: Typo in Registrations_controller.rb

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -9,7 +9,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
     if resource.persisted?
       render json: {
         status: {code: 200, message: 'Signed up successfully.'},
-        data: UserSerializer.new(current_user).serializable_hash[:data][:attributes])
+        data: UserSerializer.new(current_user).serializable_hash[:data][:attributes]
       }
     else
       render json: {
