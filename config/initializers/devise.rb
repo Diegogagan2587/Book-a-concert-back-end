e0396cb793f3781114eeafbc12688ee1a2ce0f2f
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-12-29 21:51:48 -0600

    removes config.action_mailer.default_url_options = { host: 'localhost', port: 3001 } from devise.rb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -311,5 +311,4 @@ Devise.setup do |config|
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3001 }
 end
