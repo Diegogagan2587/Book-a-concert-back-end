e8f1583278e5758ed19bfbd27561ce5032b98eba
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-12-30 01:25:46 -0600

    Fix(routes.rb) sintax error)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, path; '', path_names: {
+  devise_for :users, path: '', path_names: {
     sign_in: 'login',
     sign_out: 'logout',
     registration: 'signup'
