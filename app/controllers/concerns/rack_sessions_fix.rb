1bd092f614ca75b103dc9fe83a8d1ce14a339b76
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-12-30 12:27:19 -0600

    fix(DisabledSessionError): instruc devise to create a fake session hash

---
+++ app/controllers/concerns/rack_sessions_fix.rb
@@ -0,0 +1,16 @@
+module RackSessionsFix
+    extend ActiveSupport::Concern
+    class FakeRackSession < Hash
+      def enabled?
+        false
+      end
+      def destroy; end
+    end
+    included do
+      before_action :set_fake_session
+      private
+      def set_fake_session
+        request.env['rack.session'] ||= FakeRackSession.new
+      end
+    end
+  end
